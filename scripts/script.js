78fe078e8fdbecfaee7f43c9a1939f399104ef87
Raheel Ahmed <101556343+raheel-436@users.noreply.github.com>
2024-12-18 18:22:00 +0500

    'Dynamically displayed Orders using JS'

--- scripts/script.js
+++ scripts/script.js
@@ -25,3 +25,27 @@ themeToggler.addEventListener("click", () => {
 Explanation:
 querySelectorAll: It selects all the span elements inside .theme-toggler.
 forEach: Loops through both spans and toggles the active class on each click.*/
+
+/*Dynamic Display of tables*/
+
+Orders.forEach((order) => {
+  const tr = document.createElement("tr");
+
+  const trContent = `
+    <td>${order.productName}</td>
+    <td>${order.productNumber}</td>
+    <td>${order.paymentStatus}</td>
+    <td class="${
+      order.shipping === "Declined"
+        ? "danger"
+        : order.shipping === "Pending"
+        ? "warning"
+        : "primary"
+    }">${order.shipping}</td> 
+    <td class="primary">Details</td>
+  `;
+
+  tr.innerHTML = trContent;
+
+  document.querySelector("table tbody").appendChild(tr);
+});
